08425cb4a2a9e44cbd2c1430f2c08eb9ffc8d942
pinkyp9 <128229194+pinkyp9@users.noreply.github.com>
2023-08-28 20:35:59 +0530

    Add files via upload

--- MLtask1.2.py
+++ MLtask1.2.py
@@ -10,4 +10,22 @@ difference =[]
 for items in combinations:
     res = [i for i in lst if i not in items]
     difference.append(abs(sum(res)-sum(items)))
-print(f"the minimum difference is:{sorted(difference)[0]}")
+new_lst_index = difference.index(min(difference))
+lst1 = list(combinations[new_lst_index])
+lst2 = [i for i in lst if i not in lst1]
+lst4= lst1+lst2
+while(len(lst2)!=1):
+    sum = lst2[0]+lst2[1]
+    lst2.pop(1)
+    lst2.pop(0)
+    lst2.append(sum)
+    lst4 =lst2+lst1
+    print(sorted(lst4))
+while(len(lst1)!=1):
+    sum = lst1[0]+lst1[1]
+    lst1.pop(1)
+    lst1.pop(0)
+    lst1.append(sum)
+    lst4 = lst2+lst1
+    print(sorted(lst4))
+print(f"the minimum difference is:{abs(lst4[1]-lst4[0])}")
